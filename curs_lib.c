d6779597f4707453e9b4cacffa1e9e8ef8b482e8
Kevin McCarthy <kevin@8t8.us>
2017-09-04 09:40:44 -0700

    Fix rv assignment in mutt_window_mvprintw(). (closes #3964)
    Mutt doesn't check the return value, so this wasn't causing a bug, but
    was incorrect.
    
    Thanks to dcb314 for reporting the issue.

--- curs_lib.c
+++ curs_lib.c
@@ -665,7 +665,7 @@ int mutt_window_mvprintw (mutt_window_t *win, int row, int col, const char *fmt,
   va_list ap;
   int rv;
 
-  if ((rv = mutt_window_move (win, row, col) != ERR))
+  if ((rv = mutt_window_move (win, row, col)) != ERR)
   {
     va_start (ap, fmt);
     rv = vw_printw (stdscr, fmt, ap);
