088e1903488a6e35945763563d24f91bb0c5e6f8
Vincent Lefevre <vincent@vinc17.net>
2018-02-08 12:20:02 +0100

    Change mkreldate.sh to use the UTC date with git, so that it is increasing.

--- mkreldate.sh
+++ mkreldate.sh
@@ -3,7 +3,7 @@
 # Generates the reldate.h contents from either git or the ChangeLog file
 
 if [ -e ".git" ] && command -v git >/dev/null 2>&1; then
-  reldate=$(git log -1 --date=short --pretty=format:"%cd")
+  reldate=$(TZ=UTC git log -1 --date=format-local:"%F" --pretty=format:"%cd")
 else
   reldate=$(head -n 1 ChangeLog | LC_ALL=C cut -d ' ' -f 1)
 fi
