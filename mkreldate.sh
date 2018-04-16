4e4574c413ce7eac8cc665383fecbe8c57f89ccf
Kevin McCarthy <kevin@8t8.us>
2018-04-16 16:32:33 -0700

    Fix mkreldate.sh in master to use [-r] test too.

--- mkreldate.sh
+++ mkreldate.sh
@@ -2,7 +2,7 @@
 #
 # Generates the reldate.h contents from either git or the ChangeLog file
 
-if [ -e ".git" ] && command -v git >/dev/null 2>&1; then
+if [ -r ".git" ] && command -v git >/dev/null 2>&1; then
   reldate=$(TZ=UTC git log -1 --date=format-local:"%F" --pretty=format:"%cd")
 else
   reldate=$(head -n 1 ChangeLog | LC_ALL=C cut -d ' ' -f 1)
