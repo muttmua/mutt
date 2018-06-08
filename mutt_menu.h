ebdcfa2d5382ba6b280ae4483f3c831ebd3c96c6
Vincent Lefevre <vincent@vinc17.net>
2018-06-08 13:53:46 +0200

    Missing void in mutt_set_current_menu_redraw_full prototype

--- mutt_menu.h
+++ mutt_menu.h
@@ -127,7 +127,7 @@ void mutt_menuDestroy (MUTTMENU **);
 void mutt_push_current_menu (MUTTMENU *);
 void mutt_pop_current_menu (MUTTMENU *);
 void mutt_set_current_menu_redraw (int);
-void mutt_set_current_menu_redraw_full ();
+void mutt_set_current_menu_redraw_full (void);
 void mutt_set_menu_redraw (int, int);
 void mutt_set_menu_redraw_full (int);
 void mutt_current_menu_redraw (void);
