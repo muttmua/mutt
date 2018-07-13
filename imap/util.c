31eef6c766f47df8281942d19f76e35f475c781d
Richard Russon <rich@flatcap.org>
2018-07-13 11:33:16 -0700

    Selectively cache headers.
    Thanks to NeoMutt and Jeriko One for the patch, which was slightly
    modified to apply to the Mutt code.

--- imap/util.c
+++ imap/util.c
@@ -84,6 +84,7 @@ header_cache_t* imap_hcache_open (IMAP_DATA* idata, const char* path)
   ciss_url_t url;
   char cachepath[LONG_STRING];
   char mbox[LONG_STRING];
+  size_t len;
 
   if (path)
     imap_cachepath (idata, path, mbox, sizeof (mbox));
@@ -95,6 +96,12 @@ header_cache_t* imap_hcache_open (IMAP_DATA* idata, const char* path)
     imap_cachepath (idata, mx.mbox, mbox, sizeof (mbox));
     FREE (&mx.mbox);
   }
+
+  if (strstr(mbox, "/../") || (strcmp(mbox, "..") == 0) || (strncmp(mbox, "../", 3) == 0))
+    return NULL;
+  len = strlen(mbox);
+  if ((len > 3) && (strcmp(mbox + len - 3, "/..") == 0))
+    return NULL;
 
   mutt_account_tourl (&idata->conn->account, &url);
   url.path = mbox;
