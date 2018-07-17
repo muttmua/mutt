neomutt/neomutt

Improve OAUTHBEARER support.
Move token refresh commands to their own config variables. Consolidate
code for refreshing tokens and generating the SASL OAUTHBEARER
argument in account.c. Add support for OAUTHBEARER to pop.

Fix pop_auth_oauth() mutt_from_base64() call from 1.10.1 release.

imap/auth_oauth.c
@@ -34,8 +34,8 @@
 enum ImapAuthRes imap_auth_oauth(struct ImapData *idata, const char *method)
 {
   char *ibuf = NULL;
-  char *oauth_buf = NULL;
-  int len, ilen, oalen;
+  char *oauthbearer = NULL;
+  int ilen;
   int rc;
 
   /* For now, we only support SASL_IR also and over TLS */
@@ -45,57 +45,37 @@ enum ImapAuthRes imap_auth_oauth(struct ImapData *idata, const char *method)
 
   mutt_message(_("Authenticating (OAUTHBEARER)..."));
 
-  /* get auth info */
-  if (mutt_account_getlogin(&idata->conn->account))
+  /* We get the access token from the imap_oauth_refresh_command */
+  oauthbearer = mutt_account_getoauthbearer(&idata->conn->account);
+  if (oauthbearer == NULL)
     return IMAP_AUTH_FAILURE;
 
-  /* We get the access token from the "imap_pass" field */
-  if (mutt_account_getpass(&idata->conn->account))
-    return IMAP_AUTH_FAILURE;
-
-  /* Determine the length of the keyed message digest, add 50 for
-   * overhead.
-   */
-  oalen = mutt_str_strlen(idata->conn->account.user) +
-          mutt_str_strlen(idata->conn->account.host) +
-          mutt_str_strlen(idata->conn->account.pass) + 50;
-  oauth_buf = mutt_mem_malloc(oalen);
-
-  snprintf(oauth_buf, oalen, "n,a=%s,\001host=%s\001port=%d\001auth=Bearer %s\001\001",
-           idata->conn->account.user, idata->conn->account.host,
-           idata->conn->account.port, idata->conn->account.pass);
-
-  /* ibuf must be long enough to store the base64 encoding of
-   * oauth_buf, plus the additional debris.
-   */
-
-  ilen = mutt_str_strlen(oauth_buf) * 2 + 30;
+  ilen = mutt_str_strlen(oauthbearer) + 30;
   ibuf = mutt_mem_malloc(ilen);
-  ibuf[0] = '\0';
-
-  mutt_str_strcat(ibuf, ilen, "AUTHENTICATE OAUTHBEARER ");
-  len = mutt_str_strlen(ibuf);
-
-  mutt_b64_encode(oauth_buf, mutt_str_strlen(oauth_buf), (ibuf + len), ilen - len);
+  snprintf(ibuf, ilen, "AUTHENTICATE OAUTHBEARER %s", oauthbearer);
 
   /* This doesn't really contain a password, but the token is good for
    * an hour, so suppress it anyways.
    */
   rc = imap_exec(idata, ibuf, IMAP_CMD_FAIL_OK | IMAP_CMD_PASS);
 
-  FREE(&oauth_buf);
+  FREE(&oauthbearer);
   FREE(&ibuf);
+
+  if (rc)
+  {
+    /* The error response was in SASL continuation, so continue the SASL
+     * to cause a failure and exit SASL input.  See RFC 7628 3.2.3
+     */
+    mutt_socket_send(idata->conn, "\001");
+    rc = imap_exec(idata, ibuf, IMAP_CMD_FAIL_OK);
+  }
 
   if (!rc)
   {
     mutt_clear_error();
     return IMAP_AUTH_SUCCESS;
   }
-
-  /* The error response was in SASL continuation, so "continue" the SASL
-   * to cause a failure and exit SASL input.
-   */
-  mutt_socket_send(idata->conn, "an noop\r\n");
 
   mutt_error(_("OAUTHBEARER authentication failed."));
   mutt_sleep(2);

muttmua/mutt

imap/auth_oauth.c
@@ -31,8 +31,8 @@
 imap_auth_res_t imap_auth_oauth (IMAP_DATA* idata, const char* method)
 {
   char* ibuf = NULL;
-  char* oauth_buf = NULL;
-  int len, ilen, oalen;
+  char* oauthbearer = NULL;
+  int ilen;
   int rc;
 
   /* For now, we only support SASL_IR also and over TLS */
@@ -43,60 +43,37 @@ imap_auth_res_t imap_auth_oauth (IMAP_DATA* idata, const char* method)
 
   mutt_message _("Authenticating (OAUTHBEARER)...");
 
-  /* get auth info */
-  if (mutt_account_getlogin (&idata->conn->account))
+  /* We get the access token from the imap_oauth_refresh_command */
+  oauthbearer = mutt_account_getoauthbearer (&idata->conn->account);
+  if (oauthbearer == NULL)
     return IMAP_AUTH_FAILURE;
 
-  /* We get the access token from the "imap_pass" field */
-  if (mutt_account_getpass (&idata->conn->account))
-    return IMAP_AUTH_FAILURE;
-
-  /* Determine the length of the keyed message digest, add 50 for
-   * overhead.
-   */
-  oalen = strlen (idata->conn->account.user) +
-    strlen (idata->conn->account.host) + 
-    strlen (idata->conn->account.pass) + 50; 
-  oauth_buf = safe_malloc (oalen);
-
-  snprintf (oauth_buf, oalen,
-    "n,a=%s,\001host=%s\001port=%d\001auth=Bearer %s\001\001",
-    idata->conn->account.user, idata->conn->account.host,
-    idata->conn->account.port, idata->conn->account.pass);
-
-  /* ibuf must be long enough to store the base64 encoding of
-   * oauth_buf, plus the additional debris.
-   */
-
-  ilen = strlen (oauth_buf) * 2 + 30;
+  ilen = strlen (oauthbearer) + 30;
   ibuf = safe_malloc (ilen);
-  ibuf[0] = '\0';
-
-  safe_strcat (ibuf, ilen, "AUTHENTICATE OAUTHBEARER ");
-  len = strlen(ibuf);
-  
-  mutt_to_base64 ((unsigned char*) (ibuf + len),
-                  (unsigned char*) oauth_buf, strlen (oauth_buf),
-		  ilen - len);
+  snprintf (ibuf, ilen, "AUTHENTICATE OAUTHBEARER %s", oauthbearer);
 
   /* This doesn't really contain a password, but the token is good for
    * an hour, so suppress it anyways.
    */
   rc = imap_exec (idata, ibuf, IMAP_CMD_FAIL_OK | IMAP_CMD_PASS);
 
-  FREE (&oauth_buf);
+  FREE (&oauthbearer);
   FREE (&ibuf);
   
+  if (rc)
+  {
+    /* The error response was in SASL continuation, so continue the SASL
+     * to cause a failure and exit SASL input.  See RFC 7628 3.2.3
+     */
+    mutt_socket_write (idata->conn, "\001");
+    rc = imap_exec (idata, ibuf, IMAP_CMD_FAIL_OK);
+  }
+
   if (!rc)
   {
     mutt_clear_error();
     return IMAP_AUTH_SUCCESS;
   }
-
-  /* The error response was in SASL continuation, so "continue" the SASL
-   * to cause a failure and exit SASL input.
-   */
-  mutt_socket_write (idata->conn, "an noop\r\n");
 
   mutt_error _("OAUTHBEARER authentication failed.");
   mutt_sleep (2);