daf669183472834c9e4916d9cb7cc7b49ed1331e
Cody Brownstein <cbrownstein@liquidityllc.com>
2018-09-01 09:47:07 -0700

    Fix typo

--- init.h
+++ init.h
@@ -1356,7 +1356,7 @@ struct option_t MuttVars[] = {
   ** .pp
   ** When \fIset\fP, mutt will not open new IMAP connections to check for new
   ** mail.  Mutt will only check for new mail over existing IMAP
-  ** connections.  This is useful if you don't want to be prompted to
+  ** connections.  This is useful if you don't want to be prompted for
   ** user/password pairs on mutt invocation, or if opening the connection
   ** is slow.
   */
