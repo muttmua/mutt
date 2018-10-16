dd1e0ea4387a476ab45405c2361103162f2da294
Kevin McCarthy <kevin@8t8.us>
2018-10-16 16:04:57 -0700

    Remove conversion of nbsp to space.
    Keep the behavior of it not line-breaking, but don't change it to a
    space character when displaying.  This is so copy-paste from the pager
    will preserve the nbsp, which is semantically important in some
    locales.

--- pager.c
+++ pager.c
@@ -1216,13 +1216,6 @@ static int format_line (struct line_t **lineInfo, int n, unsigned char *buf,
     {
       if (wc == ' ')
 	space = ch;
-      else if (Charset_is_utf8 && wc == 0x00A0)
-      {
-	/* Convert non-breaking space to normal space. The local variable
-	 * `space' is not set here so that the caller of this function won't
-	 * attempt to wrap at this character. */
-	wc = ' ';
-      }
       t = wcwidth (wc);
       if (col + t > wrap_cols)
 	break;
