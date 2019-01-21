143ea851fa5ece5ba6eb89326377e1d358745048
Kevin McCarthy <kevin@8t8.us>
2019-01-21 14:16:13 -0800

    Remove mutt_socket_read().
    It is not used anywhere in the code.
    
    Additionally, it does not interact correctly with
    mutt_socket_readchar() and mutt_socket_readln_d(), which use an
    internal input buffer: conn->inbuf.
    
    If reading a specific chunksize is needed in future, the function can
    be easily recreated to use the inbuf.

--- mutt_socket.c
+++ mutt_socket.c
@@ -88,29 +88,6 @@ int mutt_socket_close (CONNECTION* conn)
   return rc;
 }
 
-int mutt_socket_read (CONNECTION* conn, char* buf, size_t len)
-{
-  int rc;
-
-  if (conn->fd < 0)
-  {
-    dprint (1, (debugfile, "mutt_socket_read: attempt to read from closed connection\n"));
-    return -1;
-  }
-
-  rc = conn->conn_read (conn, buf, len);
-  /* EOF */
-  if (rc == 0)
-  {
-    mutt_error (_("Connection to %s closed"), conn->account.host);
-    mutt_sleep (2);
-  }
-  if (rc <= 0)
-    mutt_socket_close (conn);
-
-  return rc;
-}
-
 int mutt_socket_write_d (CONNECTION *conn, const char *buf, int len, int dbg)
 {
   int rc;
