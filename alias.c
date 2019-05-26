cd30526c45ef8b5075b0f08603136ac64ab295b4
Kevin McCarthy <kevin@8t8.us>
2019-05-26 16:03:40 -0700

    Change mutt_addr_is_user() to no for a NULL address.
    See ticket #140.
    
    The behavior is generating a false 'F' flag for a spam message with an
    unparsable From address.
    
    After reviewing the callers, I can't see a reason returning 'yes' in
    this case.
    
    Commit 690c2945 fixed a potential segv issue, but the other callers
    seem robust against this change in behavior.

--- alias.c
+++ alias.c
@@ -614,11 +614,10 @@ int mutt_addr_is_user (ADDRESS *addr)
 {
   const char *fqdn;
 
-  /* NULL address is assumed to be the user. */
   if (!addr)
   {
-    dprint (5, (debugfile, "mutt_addr_is_user: yes, NULL address\n"));
-    return 1;
+    dprint (5, (debugfile, "mutt_addr_is_user: no, NULL address\n"));
+    return 0;
   }
   if (!addr->mailbox)
   {
