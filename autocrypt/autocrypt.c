e5463f577620969a19181b78b541373ced9662f6
Kevin McCarthy <kevin@8t8.us>
2019-07-27 15:34:51 -0700

    Add account->enabled checks.
    Add to ui recommendation, keylist generator, and autocrypt header
    writing routines.

--- autocrypt/autocrypt.c
+++ autocrypt/autocrypt.c
@@ -499,6 +499,9 @@ autocrypt_rec_t mutt_autocrypt_ui_recommendation (HEADER *hdr, char **keylist)
   if (mutt_autocrypt_db_account_get (hdr->env->from, &account) <= 0)
     goto cleanup;
 
+  if (!account->enabled)
+    goto cleanup;
+
   keylist_buf = mutt_buffer_pool_get ();
   mutt_buffer_addstr (keylist_buf, account->keyid);
 
@@ -593,6 +596,8 @@ int mutt_autocrypt_set_sign_as_default_key (HEADER *hdr)
     goto cleanup;
   if (!account->keyid)
     goto cleanup;
+  if (!account->enabled)
+    goto cleanup;
 
   mutt_str_replace (&AutocryptSignAs, account->keyid);
   mutt_str_replace (&AutocryptDefaultKey, account->keyid);
@@ -645,6 +650,8 @@ int mutt_autocrypt_write_autocrypt_header (ENVELOPE *env, FILE *fp)
   if (mutt_autocrypt_db_account_get (env->from, &account) <= 0)
     goto cleanup;
   if (!account->keydata)
+    goto cleanup;
+  if (!account->enabled)
     goto cleanup;
 
   fputs ("Autocrypt: ", fp);
