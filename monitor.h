472324f8e0eb32dbf3f20deecf404aa5eefa7dd4
Gero Treuner <gero@70t.de>
2019-09-01 16:58:46 +0200

    add fallback for inotify_init1

--- monitor.h
+++ monitor.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 Gero Treuer <gero@70t.de>
+ * Copyright (C) 2018 Gero Treuner <gero@70t.de>
  *
  *     This program is free software; you can redistribute it and/or modify
  *     it under the terms of the GNU General Public License as published by
