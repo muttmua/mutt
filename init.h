376716eb6fd43982abca56707b809ccd6d8741a6
Kevin McCarthy <kevin@8t8.us>
2020-02-10 14:52:32 -0800

    Revert $ssl_force_tls to default unset.
    In theory it's a good idea encourage SSL usage everywhere.
    
    However ticket #210 showed that, in reality, that kind of change
    breaks configs and so is still not a good idea.
    
    $ssl_startls defaults set and will use STARTTLS if the server
    advertises it.

--- init.h
+++ init.h
@@ -3863,7 +3863,7 @@ struct option_t MuttVars[] = {
   ** The file containing a client certificate and its associated private
   ** key.
   */
-  { "ssl_force_tls",		DT_BOOL, R_NONE, {.l=OPTSSLFORCETLS}, {.l=1} },
+  { "ssl_force_tls",		DT_BOOL, R_NONE, {.l=OPTSSLFORCETLS}, {.l=0} },
   /*
   ** .pp
   ** If this variable is \fIset\fP, Mutt will require that all connections
