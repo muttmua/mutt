69afc2f00973d748b252a0e59f23838861c9219c
Kevin McCarthy <kevin@8t8.us>
2020-07-12 10:50:51 -0700

    Try to improve "fcc to an imap mailbox" warning message.
    Add a "Warning: prefix", and print a followup message indicating the
    mailbox is being skipped.
    
    Previously, the message might leave the impression the send was
    aborted too.

--- send.c
+++ send.c
@@ -1258,7 +1258,14 @@ static void save_fcc_mailbox_part (BUFFER *fcc_mailbox, SEND_CONTEXT *sctx,
       mx_is_imap (mutt_b2s (fcc_mailbox)))
   {
     mutt_sleep (1);
-    mutt_error _ ("Fcc to an IMAP mailbox is not supported in batch mode");
+    mutt_error _("Warning: Fcc to an IMAP mailbox is not supported in batch mode");
+    /* L10N:
+       Printed after the "Fcc to an IMAP mailbox is not supported" message.
+       To make it clearer that the message doesn't mean Mutt is aborting
+       sending the mail too.
+       %s is the full mailbox URL, including imap(s)://
+    */
+    mutt_error (_("Skipping Fcc to %s"), mutt_b2s (fcc_mailbox));
     return;
   }
 #endif
