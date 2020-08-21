c0218adedc6eb420a656d9d061933d93f66126e7
Alexander Perlis <aperlis@math.lsu.edu>
2020-08-21 15:22:50 -0700

    Updates to contrib/mutt_oauth2.py and README.
    The newer version of the mutt_oauth2.py script incorporates the
    following changes:
     - Uses /usr/bin/env at top
     - Many formatting changes to appease pylint
     - Improvement to POP test error message output
    
    Also attached is a README that has instructions for both Microsoft and Google.

--- contrib/mutt_oauth2.py
+++ contrib/mutt_oauth2.py
@@ -1,6 +1,7 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 #
-# Mutt OAuth2 token management script, version 2020-07-06
+# Mutt OAuth2 token management script, version 2020-08-07
+# Written against python 3.7.3, not tried with earlier python versions.
 #
 #   Copyright (C) 2020 Alexander Perlis
 #
@@ -18,6 +19,7 @@
 #   along with this program; if not, write to the Free Software
 #   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
 #   02110-1301, USA.
+'''Mutt OAuth2 token management'''
 
 import sys
 import json
@@ -37,11 +39,13 @@ import socket
 import http.server
 import subprocess
 
-# The token file must be encrypted because it contains multi-use bearer tokens whose usage does not require
-# additional verification. Specify whichever encryption and decryption pipes you prefer. They should read
-# from standard input and write to standard output.
-ENCRYPTION_PIPE=['gpg', '--encrypt', '--recipient', 'YOUR_GPG_IDENTITY']
-DECRYPTION_PIPE=['gpg', '--decrypt']
+# The token file must be encrypted because it contains multi-use bearer tokens
+# whose usage does not require additional verification. Specify whichever
+# encryption and decryption pipes you prefer. They should read from standard
+# input and write to standard output. The example values here invoke GPG,
+# although won't work until an appropriate identity appears in the first line.
+ENCRYPTION_PIPE = ['gpg', '--encrypt', '--recipient', 'YOUR_GPG_IDENTITY']
+DECRYPTION_PIPE = ['gpg', '--decrypt']
 
 registrations = {
     'google': {
@@ -67,7 +71,9 @@ registrations = {
         'pop_endpoint': 'outlook.office365.com',
         'smtp_endpoint': 'smtp.office365.com',
         'sasl_method': 'XOAUTH2',
-        'scope': 'offline_access https://outlook.office.com/IMAP.AccessAsUser.All https://outlook.office.com/POP.AccessAsUser.All https://outlook.office.com/SMTP.Send',
+        'scope': ('offline_access https://outlook.office.com/IMAP.AccessAsUser.All '
+                  'https://outlook.office.com/POP.AccessAsUser.All '
+                  'https://outlook.office.com/SMTP.Send'),
         'client_id': '',
         'client_secret': '',
     },
@@ -91,28 +97,36 @@ args = ap.parse_args()
 token = {}
 path = Path(args.tokenfile)
 if path.exists():
-    if 0o600 != 0o777 & path.stat().st_mode:
+    if 0o777 & path.stat().st_mode != 0o600:
         sys.exit('Token file has unsafe mode. Suggest deleting and starting over.')
     try:
-        sub = subprocess.run(DECRYPTION_PIPE, check=True, input=path.read_bytes(), capture_output=True)
+        sub = subprocess.run(DECRYPTION_PIPE, check=True, input=path.read_bytes(),
+                             capture_output=True)
         token = json.loads(sub.stdout)
     except subprocess.CalledProcessError:
-        sys.exit('Difficulty decrypting token file. Is your decryption agent primed for non-interactive usage, or an appropriate environment variable such as GPG_TTY set to allow interactive agent usage from inside a pipe?')
+        sys.exit('Difficulty decrypting token file. Is your decryption agent primed for '
+                 'non-interactive usage, or an appropriate environment variable such as '
+                 'GPG_TTY set to allow interactive agent usage from inside a pipe?')
 
 def writetokenfile():
-    if not path.exists(): path.touch(mode=0o600)
-    if 0o600 != 0o777 & path.stat().st_mode:
+    '''Writes global token dictionary into token file.'''
+    if not path.exists():
+        path.touch(mode=0o600)
+    if 0o777 & path.stat().st_mode != 0o600:
         sys.exit('Token file has unsafe mode. Suggest deleting and starting over.')
-    sub = subprocess.run(ENCRYPTION_PIPE, check=True, input=json.dumps(token).encode(), capture_output=True)
-    path.write_bytes(sub.stdout)
+    sub2 = subprocess.run(ENCRYPTION_PIPE, check=True, input=json.dumps(token).encode(),
+                          capture_output=True)
+    path.write_bytes(sub2.stdout)
 
-if args.debug: print('Obtained from token file:', json.dumps(token))
+if args.debug:
+    print('Obtained from token file:', json.dumps(token))
 if not token:
     if not args.authorize:
         sys.exit('You must run script with "--authorize" at least once.')
     print('Available app and endpoint registrations:', *registrations)
     token['registration'] = input('OAuth2 registration: ')
-    token['authflow'] = input('Preferred OAuth2 flow ("authcode" or "localhostauthcode" or "devicecode"): ')
+    token['authflow'] = input('Preferred OAuth2 flow ("authcode" or "localhostauthcode" '
+                              'or "devicecode"): ')
     token['email'] = input('Account e-mail address: ')
     token['access_token'] = ''
     token['access_token_expiration'] = ''
@@ -120,30 +134,38 @@ if not token:
     writetokenfile()
 
 if token['registration'] not in registrations:
-    sys.exit(f'ERROR: Unknown registration "{token["registration"]}". Delete token file and start over.')
+    sys.exit(f'ERROR: Unknown registration "{token["registration"]}". Delete token file '
+             f'and start over.')
 registration = registrations[token['registration']]
 
 authflow = token['authflow']
-if args.authflow: authflow = args.authflow
+if args.authflow:
+    authflow = args.authflow
 
 baseparams = {'client_id': registration['client_id']}
 # Microsoft uses 'tenant' but Google does not
-if 'tenant' in registration: baseparams['tenant'] = registration['tenant']
+if 'tenant' in registration:
+    baseparams['tenant'] = registration['tenant']
 
 def access_token_valid():
-    a = token['access_token_expiration']
-    return a and datetime.now() < datetime.fromisoformat(a)
+    '''Returns True when stored access token exists and is still valid at this time.'''
+    token_exp = token['access_token_expiration']
+    return token_exp and datetime.now() < datetime.fromisoformat(token_exp)
 
 def update_tokens(r):
+    '''Takes a response dictionary, extracts tokens out of it, and updates token file.'''
     token['access_token'] = r['access_token']
-    token['access_token_expiration'] = (datetime.now() + timedelta(seconds=int(r['expires_in']))).isoformat()
-    if 'refresh_token' in r: token['refresh_token'] = r['refresh_token']
+    token['access_token_expiration'] = (datetime.now() +
+                                        timedelta(seconds=int(r['expires_in']))).isoformat()
+    if 'refresh_token' in r:
+        token['refresh_token'] = r['refresh_token']
     writetokenfile()
-    if args.verbose: print(f'NOTICE: Obtained new access token, expires {token["access_token_expiration"]}.')
+    if args.verbose:
+        print(f'NOTICE: Obtained new access token, expires {token["access_token_expiration"]}.')
 
 
 if args.authorize:
-    p = baseparams.copy();
+    p = baseparams.copy()
     p['scope'] = registration['scope']
 
     if authflow in ('authcode', 'localhostauthcode'):
@@ -159,71 +181,86 @@ if args.authorize:
             s.close()
             redirect_uri = 'http://localhost:'+str(port)+'/'
             # Probably should edit the port number into the actual redirect URL.
-            
+
         p.update({'login_hint': token['email'],
                   'response_type': 'code',
                   'redirect_uri': redirect_uri,
                   'code_challenge': challenge,
                   'code_challenge_method': 'S256'})
-        print(registration["authorize_endpoint"]+'?'+urllib.parse.urlencode(p, quote_via=urllib.parse.quote))
+        print(registration["authorize_endpoint"] + '?' +
+              urllib.parse.urlencode(p, quote_via=urllib.parse.quote))
 
         authcode = ''
         if authflow == 'authcode':
-            authcode = input('Visit displayed URL to retrieve authorization code. Enter code from server (might be in browser address bar): ')
+            authcode = input('Visit displayed URL to retrieve authorization code. Enter '
+                             'code from server (might be in browser address bar): ')
         else:
-            print('Visit displayed URL to authorize this application. Waiting...', end='', flush=True)
+            print('Visit displayed URL to authorize this application. Waiting...',
+                  end='', flush=True)
             class MyHandler(http.server.BaseHTTPRequestHandler):
+                '''Handles the browser query resulting from redirect to redirect_uri.'''
                 def do_HEAD(self):
+                    '''Response to a HEAD requests.'''
                     self.send_response(200)
                     self.send_header('Content-type', 'text/html')
                     self.end_headers()
                 def do_GET(self):
+                    '''For GET request, extract code parameter from URL.'''
                     global authcode
                     querystring = urllib.parse.urlparse(self.path).query
                     querydict = urllib.parse.parse_qs(querystring)
-                    if 'code' in querydict: authcode = querydict['code'][0]
+                    if 'code' in querydict:
+                        authcode = querydict['code'][0]
                     self.do_HEAD()
                     self.wfile.write(b'<html><head><title>Authorizaton result</title></head>')
-                    self.wfile.write(b'<body><p>Authorization redirect completed. You may close this window.</p></body></html>')
-            with http.server.HTTPServer(('127.0.0.1',port), MyHandler) as httpd:
-                try: httpd.handle_request()
-                except KeyboardInterrupt: pass
-        
+                    self.wfile.write(b'<body><p>Authorization redirect completed. You may '
+                                     b'close this window.</p></body></html>')
+            with http.server.HTTPServer(('127.0.0.1', port), MyHandler) as httpd:
+                try:
+                    httpd.handle_request()
+                except KeyboardInterrupt:
+                    pass
+
         if not authcode:
             sys.exit('Did not obtain an authcode.')
 
-        for k in 'response_type', 'login_hint', 'code_challenge', 'code_challenge_method': del p[k]
+        for k in 'response_type', 'login_hint', 'code_challenge', 'code_challenge_method':
+            del p[k]
         p.update({'grant_type': 'authorization_code',
                   'code': authcode,
                   'client_secret': registration['client_secret'],
                   'code_verifier': verifier})
         try:
             response = urllib.request.urlopen(registration['token_endpoint'],
-                                      urllib.parse.urlencode(p).encode())
+                                              urllib.parse.urlencode(p).encode())
         except urllib.error.HTTPError as err:
             print(err.code, err.reason)
             response = err
         response = response.read()
-        if args.debug: print(response)
+        if args.debug:
+            print(response)
         response = json.loads(response)
         if 'error' in response:
             print(response['error'])
-            if 'error_description' in response: print(response['error_description'])
+            if 'error_description' in response:
+                print(response['error_description'])
             sys.exit(1)
 
     elif authflow == 'devicecode':
         try:
             response = urllib.request.urlopen(registration['devicecode_endpoint'],
-                                      urllib.parse.urlencode(p).encode())
+                                              urllib.parse.urlencode(p).encode())
         except urllib.error.HTTPError as err:
             print(err.code, err.reason)
             response = err
         response = response.read()
-        if args.debug: print(response)
+        if args.debug:
+            print(response)
         response = json.loads(response)
         if 'error' in response:
             print(response['error'])
-            if 'error_description' in response: print(response['error_description'])
+            if 'error_description' in response:
+                print(response['error_description'])
             sys.exit(1)
         print(response['message'])
         del p['scope']
@@ -237,14 +274,16 @@ if args.authorize:
             print('.', end='', flush=True)
             try:
                 response = urllib.request.urlopen(registration['token_endpoint'],
-                                      urllib.parse.urlencode(p).encode())
+                                                  urllib.parse.urlencode(p).encode())
             except urllib.error.HTTPError as err:
                 # Not actually always an error, might just mean "keep trying..."
                 response = err
             response = response.read()
-            if args.debug: print(response)
+            if args.debug:
+                print(response)
             response = json.loads(response)
-            if 'error' not in response: break
+            if 'error' not in response:
+                break
             if response['error'] == 'authorization_declined':
                 print(' user declined authorization.')
                 sys.exit(1)
@@ -253,18 +292,22 @@ if args.authorize:
                 sys.exit(1)
             if response['error'] != 'authorization_pending':
                 print(response['error'])
-                if 'error_description' in response: print(response['error_description'])
+                if 'error_description' in response:
+                    print(response['error_description'])
                 sys.exit(1)
         print()
 
     else:
-        sys.exit(f'ERROR: Unknown OAuth2 flow "{token["authflow"]}. Delete token file and start over.')
+        sys.exit(f'ERROR: Unknown OAuth2 flow "{token["authflow"]}. Delete token file and '
+                 f'start over.')
 
     update_tokens(response)
 
 
 if not access_token_valid():
-    if args.verbose: print('NOTICE: Invalid or expired access token; using refresh token to obtain new access token.')
+    if args.verbose:
+        print('NOTICE: Invalid or expired access token; using refresh token '
+              'to obtain new access token.')
     if not token['refresh_token']:
         sys.exit(f'ERROR: No refresh token. Run script with "--authorize".')
     p = baseparams.copy()
@@ -273,16 +316,18 @@ if not access_token_valid():
               'grant_type': 'refresh_token'})
     try:
         response = urllib.request.urlopen(registration['token_endpoint'],
-                                      urllib.parse.urlencode(p).encode())
+                                          urllib.parse.urlencode(p).encode())
     except urllib.error.HTTPError as err:
         print(err.code, err.reason)
         response = err
     response = response.read()
-    if args.debug: print(response)
+    if args.debug:
+        print(response)
     response = json.loads(response)
     if 'error' in response:
         print(response['error'])
-        if 'error_description' in response: print(response['error_description'])
+        if 'error_description' in response:
+            print(response['error_description'])
         print(f'Perhaps refresh token invalid. Try running once with "--authorize"')
         sys.exit(1)
     update_tokens(response)
@@ -292,24 +337,28 @@ if not access_token_valid():
     sys.exit('ERROR: No valid access token. This should not be able to happen.')
 
 
-if args.verbose: print('Access Token: ', end='')
+if args.verbose:
+    print('Access Token: ', end='')
 print(token['access_token'])
-    
 
-def build_sasl_string(user,host,port,token):
+
+def build_sasl_string(user, host, port, token):
+    '''Build appropriate SASL string, which depends on cloud server's supported SASL method.'''
     if registration['sasl_method'] == 'OAUTHBEARER':
         return f'n,a={user},\1host={host}\1port={port}\1auth=Bearer {token}\1\1'
-    elif registration['sasl_method'] == 'XOAUTH2':
+    if registration['sasl_method'] == 'XOAUTH2':
         return f'user={user}\1auth=Bearer {token}\1\1'
-    else: sys.exit(f'Unknown SASL method {registration["sasl_method"]}.')
+    sys.exit(f'Unknown SASL method {registration["sasl_method"]}.')
 
-    
+
 if args.test:
     errors = False
 
     imap_conn = imaplib.IMAP4_SSL(registration['imap_endpoint'])
-    sasl_string = build_sasl_string(token['email'], registration['imap_endpoint'], 993, token['access_token'])
-    if args.debug: imap_conn.debug = 4
+    sasl_string = build_sasl_string(token['email'], registration['imap_endpoint'], 993,
+                                    token['access_token'])
+    if args.debug:
+        imap_conn.debug = 4
     try:
         imap_conn.authenticate(registration['sasl_method'], lambda _: sasl_string.encode())
         # Microsoft has a bug wherein a mismatch between username and token can still report a
@@ -317,37 +366,45 @@ if args.test:
         # Fortunately subsequent commands fail with an error. Thus we follow AUTH with another
         # IMAP command before reporting success.
         imap_conn.list()
-        if args.verbose: print('IMAP authentication succeeded')
+        if args.verbose:
+            print('IMAP authentication succeeded')
     except imaplib.IMAP4.error as e:
         print('IMAP authentication FAILED (does your account allow IMAP?):', e)
         errors = True
 
     pop_conn = poplib.POP3_SSL(registration['pop_endpoint'])
-    sasl_string = build_sasl_string(token['email'], registration['pop_endpoint'], 995, token['access_token'])
-    if args.debug: pop_conn.set_debuglevel(2)
+    sasl_string = build_sasl_string(token['email'], registration['pop_endpoint'], 995,
+                                    token['access_token'])
+    if args.debug:
+        pop_conn.set_debuglevel(2)
     try:
         # poplib doesn't have an auth command taking an authenticator object
         # Microsoft requires a two-line SASL for POP
         pop_conn._shortcmd('AUTH ' + registration['sasl_method'])
         pop_conn._shortcmd(base64.standard_b64encode(sasl_string.encode()).decode())
-        if args.verbose: print('POP authentication succeeded')
+        if args.verbose:
+            print('POP authentication succeeded')
     except poplib.error_proto as e:
-        print('POP authentication FAILED (does your account allow POP?):', e)
+        print('POP authentication FAILED (does your account allow POP?):', e.args[0].decode())
         errors = True
 
     # SMTP_SSL would be simpler but Microsoft does not answer on port 465.
     smtp_conn = smtplib.SMTP(registration['smtp_endpoint'], 587)
-    sasl_string = build_sasl_string(token['email'], registration['smtp_endpoint'], 587, token['access_token'])
+    sasl_string = build_sasl_string(token['email'], registration['smtp_endpoint'], 587,
+                                    token['access_token'])
     smtp_conn.ehlo('test')
     smtp_conn.starttls()
     smtp_conn.ehlo('test')
-    if args.debug: smtp_conn.set_debuglevel(2)
+    if args.debug:
+        smtp_conn.set_debuglevel(2)
     try:
-        smtp_conn.auth(registration['sasl_method'], lambda _ = None: sasl_string)
-        if args.verbose: print('SMTP authentication succeeded')
+        smtp_conn.auth(registration['sasl_method'], lambda _=None: sasl_string)
+        if args.verbose:
+            print('SMTP authentication succeeded')
     except smtplib.SMTPAuthenticationError as e:
         print('SMTP authentication FAILED:', e)
         errors = True
 
-    if errors: sys.exit(1)
+    if errors:
+        sys.exit(1)
 
