9ef96f1f33374acb1a0777838ab04c3351a309c1
Remco Rĳnders <remco@webconquest.com>
2020-08-25 12:23:37 -0400

    Remove always true conditional (#if 1)

--- strcasestr.c
+++ strcasestr.c
@@ -12,7 +12,6 @@ char *strcasestr(const char *s1, const char *s2)
         register const char *s = s1;
         register const char *p = s2;
 
-#if 1
         do {
                 if (!*p) {
                         return (char *) s1;;
@@ -30,19 +29,4 @@ char *strcasestr(const char *s1, const char *s2)
                         s = ++s1;
                 }
         } while (1);
-#else
-        while (*p && *s) {
-                if ((*p == *s)
-                        || (tolower(*((unsigned char *)p)) == tolower(*((unsigned char *)s)))
-                        ) {
-                        ++p;
-                        ++s;
-                } else {
-                        p = s2;
-                        s = ++s1;
-                }
-        }
-
-        return (*p) ? NULL : (char *) s1;
-#endif
 }
