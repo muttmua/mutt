2b30e41dd2b475699ad937a943ed9c85ddfe9be2
Kevin McCarthy <kevin@8t8.us>
2020-08-27 15:03:08 -0700

    Remove cur parameter for pager recall and make-key ops.
    For the recall operation, this can lead to a memory leak where the
    recalled message overwrites the sctx saved data.
    
    Arguably instead, the send_message setup could be more picky about
    saving cur, or recall-message could be careful to not overwrite the
    message-id.
    
    The context parameter isn't actually used for mail-key, and the
    curs_main.c doesn't pass it.  But I can't bring myself to make that
    change for a stable fix.

--- pager.c
+++ pager.c
@@ -2691,7 +2691,7 @@ search_next:
 	CHECK_MODE(IsHeader (extra) && !IsAttach(extra));
         CHECK_ATTACH;
 	mutt_send_message (SENDPOSTPONED | SENDBACKGROUNDEDIT,
-                           NULL, NULL, extra->ctx, extra->hdr);
+                           NULL, NULL, extra->ctx, NULL);
 	pager_menu->redraw = REDRAW_FULL;
 	break;
 
@@ -2879,7 +2879,7 @@ search_next:
         }
 	CHECK_MODE(IsHeader(extra));
         CHECK_ATTACH;
-	mutt_send_message (SENDKEY, NULL, NULL, extra->ctx, extra->hdr);
+	mutt_send_message (SENDKEY, NULL, NULL, extra->ctx, NULL);
 	pager_menu->redraw = REDRAW_FULL;
 	break;
 
