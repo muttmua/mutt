f8e405b3f0b95c15c98b5377928ca4b569f7a924
Kevin McCarthy <kevin@8t8.us>
2021-04-19 14:48:01 -0700

    Revert "Add type resolution when searching in the pager."
    This reverts commit aa308b67a983918eb59f1d7b5af543817c2cfd04.
    
    This may be what I want to push to master eventually, but this commit
    was accidental.  I need to test it more on a branch and add a better
    commit message before commiting to master.

--- pager.c
+++ pager.c
@@ -2398,7 +2398,7 @@ search_next:
 	  /* update the search pointers */
 	  i = 0;
 	  while (display_line (rd.fp, &rd.last_pos, &rd.lineInfo, i, &rd.lastLine,
-                               &rd.maxLine, MUTT_SEARCH | (flags & MUTT_PAGER_NSKIP) | (flags & MUTT_PAGER_NOWRAP) | rd.has_types,
+                               &rd.maxLine, MUTT_SEARCH | (flags & MUTT_PAGER_NSKIP) | (flags & MUTT_PAGER_NOWRAP),
                                &rd.QuoteList, &rd.q_level,
                                &rd.force_redraw, &rd.SearchRE, rd.pager_window) == 0)
 	    i++;
