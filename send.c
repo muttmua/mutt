muttmua/mutt

commit 961cf4bed679d9495cc3682d18496a53061d4581

Apply $reverse_name when tag-reply/forwarding too.
Enable $reverse_name for tag actions in the attach menu too.  Since
the ATTACH_CONTEXT is a different structure, move both single and
tagged action handling into recvcmd.c

send.c
@@ -1052,29 +1052,24 @@ void mutt_set_followup_to (ENVELOPE *e)
   }
 }
 
-
 /* look through the recipients of the message we are replying to, and if
    we find an address that matches $alternates, we use that as the default
    from field */
-static ADDRESS *set_reverse_name (ENVELOPE *env)
+static ADDRESS *set_reverse_name (SEND_CONTEXT *sctx, CONTEXT *ctx)
 {
-  ADDRESS *tmp;
+  ADDRESS *tmp = NULL;
+  int i;
 
-  for (tmp = env->to; tmp; tmp = tmp->next)
+  if (sctx->cur)
+    tmp = mutt_find_user_in_envelope (sctx->cur->env);
+  else if (ctx && ctx->tagged)
   {
-    if (mutt_addr_is_user (tmp))
-      break;
+    for (i = 0; i < ctx->vcount; i++)
+      if (ctx->hdrs[ctx->v2r[i]]->tagged)
+        if ((tmp = mutt_find_user_in_envelope (ctx->hdrs[ctx->v2r[i]]->env)) != NULL)
+          break;
   }
-  if (!tmp)
-  {
-    for (tmp = env->cc; tmp; tmp = tmp->next)
-    {
-      if (mutt_addr_is_user (tmp))
-	break;
-    }
-  }
-  if (!tmp && mutt_addr_is_user (env->from))
-    tmp = env->from;
+
   if (tmp)
   {
     tmp = rfc822_cpy_adr_real (tmp);
@@ -1956,7 +1951,8 @@ static int send_message_setup (SEND_CONTEXT *sctx, const char *tempfile,
   }
 
   /* this is handled here so that the user can match ~f in send-hook */
-  if (sctx->cur && option (OPTREVNAME) && !(sctx->flags & (SENDPOSTPONED|SENDRESEND)))
+  if (option (OPTREVNAME) && ctx &&
+      (sctx->flags & (SENDREPLY | SENDFORWARD | SENDTOSENDER)))
   {
     /* we shouldn't have to worry about freeing `sctx->msg->env->from' before
      * setting it here since this code will only execute when doing some
@@ -1970,7 +1966,7 @@ static int send_message_setup (SEND_CONTEXT *sctx, const char *tempfile,
      * have their aliases expanded.
      */
 
-    sctx->msg->env->from = set_reverse_name (sctx->cur->env);
+    sctx->msg->env->from = set_reverse_name (sctx, ctx);
   }
 
   if (! (sctx->flags & (SENDPOSTPONED|SENDRESEND)) &&

alias.c
@@ -682,3 +682,24 @@ int mutt_addr_is_user (ADDRESS *addr)
   dprint (5, (debugfile, "mutt_addr_is_user: no, all failed.\n"));
   return 0;
 }
+
+ADDRESS *mutt_find_user_in_envelope (ENVELOPE *env)
+{
+  ADDRESS *tmp;
+
+  if (!env)
+    return NULL;
+
+  for (tmp = env->to; tmp; tmp = tmp->next)
+    if (mutt_addr_is_user (tmp))
+      return tmp;
+
+  for (tmp = env->cc; tmp; tmp = tmp->next)
+    if (mutt_addr_is_user (tmp))
+      return tmp;
+
+  if (mutt_addr_is_user (env->from))
+    return env->from;
+
+  return NULL;
+}

protos.h
@@ -421,6 +421,8 @@ pid_t mutt_create_filter_fd (const char *, FILE **, FILE **, FILE **, int, int, 
 
 ADDRESS *alias_reverse_lookup (ADDRESS *);
 
+ADDRESS *mutt_find_user_in_envelope (ENVELOPE *env);
+
 /* lib.c files transplanted to muttlib.c */
 int mutt_rmtree (const char *);
 FILE *safe_fopen (const char *, const char *);

recvcmd.c
@@ -113,6 +113,49 @@ static short count_tagged_children (ATTACH_CONTEXT *actx, short i)
   return count;
 }
 
+/* Look through the recipients of the message we are replying to, and if
+ * we find an address that matches $alternates, we use that as the default
+ * from field.
+ *
+ * This is partically duplicated logic from set_reverse_name() but
+ * with with different data structures.  Also, in the attach case, we
+ * can have both cur and actx non-NULL in some cases.
+ */
+static ADDRESS *attach_set_reverse_name (HEADER *cur, ATTACH_CONTEXT *actx)
+{
+  ADDRESS *tmp = NULL;
+  int i;
+
+  if (cur)
+    tmp = mutt_find_user_in_envelope (cur->env);
+
+  /* Note that, for replying, we can have a case where both cur and act are
+   * not null, so don't use an 'else' for the second test */
+  if (!tmp && actx)
+  {
+    for (i = 0; i < actx->idxlen; i++)
+      if (actx->idx[i]->content->tagged && actx->idx[i]->content->hdr)
+        if ((tmp = mutt_find_user_in_envelope (actx->idx[i]->content->hdr->env)) != NULL)
+          break;
+  }
+
+  if (tmp)
+  {
+    tmp = rfc822_cpy_adr_real (tmp);
+    /* when $reverse_realname is not set, clear the personal name so that it
+     * may be set vi a reply- or send-hook.
+     */
+    if (!option (OPTREVREAL))
+    {
+      FREE (&tmp->personal);
+#ifdef EXACT_ADDRESS
+      FREE (&tmp->val);
+#endif
+    }
+  }
+  return (tmp);
+}
+
 
 
 /**
@@ -554,6 +597,9 @@ static void attach_forward_bodies (FILE * fp, HEADER * hdr,
 
   safe_fclose (&tmpfp);
 
+  if (option (OPTREVNAME))
+    tmphdr->env->from = attach_set_reverse_name (parent_hdr, NULL);
+
   /* now that we have the template, send it. */
   mutt_send_message (SENDBACKGROUNDEDIT, tmphdr, mutt_b2s (tmpbody), NULL,
                      parent_hdr);
@@ -685,6 +731,10 @@ static void attach_forward_msgs (FILE * fp, HEADER * hdr,
   else
     mutt_free_header (&tmphdr);
 
+  if (option (OPTREVNAME))
+    tmphdr->env->from = attach_set_reverse_name (cur ? curhdr : NULL,
+                                                 cur ? NULL : actx);
+
   mutt_send_message (SENDBACKGROUNDEDIT, tmphdr,
                      mutt_buffer_len (tmpbody) ? mutt_b2s (tmpbody) : NULL,
                      NULL, curhdr);
@@ -743,6 +793,11 @@ void mutt_attach_mail_sender (FILE *fp, HEADER *hdr, ATTACH_CONTEXT *actx,
 	return;
     }
   }
+
+  if (option (OPTREVNAME))
+    tmphdr->env->from = attach_set_reverse_name (cur ? cur->hdr : NULL,
+                                                 cur ? NULL : actx);
+
   mutt_send_message (SENDBACKGROUNDEDIT, tmphdr, NULL, NULL, NULL);
 }
 
@@ -991,6 +1046,11 @@ void mutt_attach_reply (FILE * fp, HEADER * hdr,
   }
 
   safe_fclose (&tmpfp);
+
+  if (option (OPTREVNAME))
+    tmphdr->env->from = attach_set_reverse_name (parent_hdr ? parent_hdr :
+                                                 (cur ? cur->hdr : NULL),
+                                                 cur ? NULL : actx);
 
   if (mutt_send_message (flags, tmphdr, mutt_b2s (tmpbody), NULL,
                          parent_hdr ? parent_hdr : (cur ? cur->hdr : NULL)) == 0)

commit 36fb911fccb5eb3c625a98a844d192d06ece2216

Do not set From reverse name on postponed or to be resend messages
Messages to be resend or resumed from postponed already had
reverse names and alternates evaluated the first time they entered
the compose menu and send-hook were already executed then. The
user may also had edited the From to any other content before
postponing or sending the message. Upon resuming, do not overwrite
with a reverse_name lookup and do not remove the real name if
reverse_realname=no is set.

This restores the old behaviour and fixes the regression
introduced with

    commit 961cf4bed679d9495cc3682d18496a53061d4581
    CommitDate: Mon Feb 1 14:00:56 2021 -0800

        Apply $reverse_name when tag-reply/forwarding too.

send.c
@@ -1964,6 +1964,7 @@ static int send_message_setup (SEND_CONTEXT *sctx, const char *tempfile,
 
   /* this is handled here so that the user can match ~f in send-hook */
   if (option (OPTREVNAME) && ctx &&
+      !(sctx->flags & (SENDPOSTPONED|SENDRESEND)) &&
       (sctx->flags & (SENDREPLY | SENDFORWARD | SENDTOSENDER)))
   {
     /* we shouldn't have to worry about freeing `sctx->msg->env->from' before