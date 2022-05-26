ca9602284a2854006143cea3cc00fcdf5d25976c
Kevin McCarthy <kevin@8t8.us>
2022-05-25 20:59:39 -0700

    Fix header folding my_width() calculation.
    After calculating the width of a character, the routine would only
    increment the string pointer by one byte.  Any errors returned by
    mbtowc() would also increment the width by one.  This means multibyte
    characters would overcount width by the number of bytes minus one.
    
    Change it to check the return value and use that value to increment
    the string pointer.
    
    Change mbtowc() to look at the whole rest of the string instead of
    just MB_CUR_MAX, as the manpage says even MB_CUR_MAX may not be enough
    in some circumstances.
    
    Since we calculate strlen, use that as well as '\0' for the loop
    termination check.  Also check for mbtowc() returning 0 just for extra
    safety.
    
    Reset the internal mbstate_t before converting, and on any error.
    
    If mbtowc() returns an error, use replacment_char() as a substitue for
    width calcluation, as mutt_strwidth() and other parts of Mutt do.

--- sendlib.c
+++ sendlib.c
@@ -1881,36 +1881,49 @@ static const char *find_word (const char *src)
 }
 
 /* like wcwidth(), but gets const char* not wchar_t* */
-static int my_width (const char *str, int col, int flags)
+static int my_width (const char *p, int col, int flags)
 {
   wchar_t wc;
-  int l, w = 0, nl = 0;
-  const char *p = str;
+  int l, w = 0, nl = 0, consumed;
+  size_t n;
 
-  while (p && *p)
+  if (!p)
+    return 0;
+
+  mbtowc (NULL, NULL, 0);
+  n = mutt_strlen (p);
+
+  while (*p && n)
   {
-    if (mbtowc (&wc, p, MB_CUR_MAX) >= 0)
+    consumed = mbtowc (&wc, p, n);
+    if (!consumed)
+      break;
+    if (consumed < 0)
     {
-      l = wcwidth (wc);
-      if (l < 0)
-	l = 1;
-      /* correctly calc tab stop, even for sending as the
-       * line should look pretty on the receiving end */
-      if (wc == L'\t' || (nl && wc == L' '))
-      {
-	nl = 0;
-	l = 8 - (col % 8);
-      }
-      /* track newlines for display-case: if we have a space
-       * after a newline, assume 8 spaces as for display we
-       * always tab-fold */
-      else if ((flags & CH_DISPLAY) && wc == '\n')
-	nl = 1;
+      mbtowc (NULL, NULL, 0);
+      wc = replacement_char ();
+      consumed = 1;
     }
-    else
+
+    l = wcwidth (wc);
+    if (l < 0)
       l = 1;
+    /* correctly calc tab stop, even for sending as the
+     * line should look pretty on the receiving end */
+    if (wc == L'\t' || (nl && wc == L' '))
+    {
+      nl = 0;
+      l = 8 - (col % 8);
+    }
+    /* track newlines for display-case: if we have a space
+     * after a newline, assume 8 spaces as for display we
+     * always tab-fold */
+    else if ((flags & CH_DISPLAY) && wc == '\n')
+      nl = 1;
+
     w += l;
-    p++;
+    p += consumed;
+    n -= consumed;
   }
   return w;
 }
