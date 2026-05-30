60a50888420af32cf94b57fda84274c0481273c3
Grzegorz Szymaszek <gszymaszek@short.pl>
2026-05-30 16:03:19 +0200

    Fix formatting of a few error messages in crypt-gpgme.c
    Those were broken in f31b98e7e22a16867584af2e82d46c29917bfd83
    ("Formatting update: remove space between function and parenthesis."),
    likely because "ID (unknown encoding)" may look like a function call.

--- crypt-gpgme.c
+++ crypt-gpgme.c
@@ -3814,14 +3814,14 @@ parse_and_print_user_id(FILE *fp, const char *userid)
         print_utf8(fp, userid+1, s-userid-1);
     }
   else if (*userid == '(')
-    fputs(_("[Can't display this user ID(unknown encoding)]"), fp);
+    fputs(_("[Can't display this user ID (unknown encoding)]"), fp);
   else if (!digit_or_letter ((const unsigned char *)userid))
-    fputs(_("[Can't display this user ID(invalid encoding)]"), fp);
+    fputs(_("[Can't display this user ID (invalid encoding)]"), fp);
   else
     {
       struct dn_array_s *dn = parse_dn((const unsigned char *)userid);
       if (!dn)
-        fputs(_("[Can't display this user ID(invalid DN)]"), fp);
+        fputs(_("[Can't display this user ID (invalid DN)]"), fp);
       else
         {
           print_dn_parts(fp, dn);
